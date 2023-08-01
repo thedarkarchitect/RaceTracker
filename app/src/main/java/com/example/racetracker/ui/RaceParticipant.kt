b03cf91600e447dab5b499fa60b3468b65b258a1
thedarkarchitect <romanreynolds240@gmail.com>
2023-08-01 07:45:20 +0300

    kotlin coroutine scope implementation done

--- app/src/main/java/com/example/racetracker/ui/RaceParticipant.kt
+++ app/src/main/java/com/example/racetracker/ui/RaceParticipant.kt
@@ -4,6 +4,7 @@ package com.example.racetracker.ui
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import kotlinx.coroutines.delay
 
 /**
  * This class represents a state holder for race participant.
@@ -25,6 +26,14 @@ class RaceParticipant(
      */
     var currentProgress by mutableStateOf(initialProgress)
         private set
+
+    suspend fun run(){
+        while (currentProgress < maxProgress){
+            delay(progressDelayMillis)
+            currentProgress += progressIncrement
+
+        }
+    }
 
     /**
      * Regardless of the value of [initialProgress] the reset function will reset the
--- app/src/main/java/com/example/racetracker/ui/RaceTrackerApp.kt
+++ app/src/main/java/com/example/racetracker/ui/RaceTrackerApp.kt
@@ -18,6 +18,7 @@ import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -32,6 +33,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.racetracker.R
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 @Composable
 fun RaceTrackerApp() {
@@ -48,6 +51,18 @@ fun RaceTrackerApp() {
     }
     var raceInProgress by remember { mutableStateOf(false) }
 
+    if(raceInProgress) {
+        LaunchedEffect(playerOne, playerTwo) {
+            //this is a coroutine that runs the suspending function as long as it remains in the composition
+            //launchedEffect stops when recomposition of composable happens
+            coroutineScope{
+                launch { playerOne.run() }
+                launch { playerTwo.run() }
+            }
+            raceInProgress = false
+        }
+    }
+
     RaceTrackerScreen(
         playerOne = playerOne,
         playerTwo = playerTwo,
@@ -55,6 +70,7 @@ fun RaceTrackerApp() {
         onRunStateChange = { raceInProgress = it }
     )
 }
+
 
 @Composable
 private fun RaceTrackerScreen(
